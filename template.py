e8ee8552e4cdd2a5d7119f7c58f1c5bf42d084b7
Nandan1041996 <mayurnandanwar@ghcl.co.in>
2026-01-19 14:32:57 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,49 @@
+import os 
+from pathlib import Path 
+import logging 
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(astime)s]: %(message)s:')
+
+
+project_name = "cnnClassifier"
+
+list_of_files = [
+    ".github/wokflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py"
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials/ipynb"
+]
+# why .gitkeep if you commit just empty folder then it will not reflact on github, something should be in folder not empty.
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir != '':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory: {filedir}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            
+            logging.info(f"creating empty file:{filepath}")
+
+
+    else:
+        logging.info("filename is already exist")
+
+
+
+
